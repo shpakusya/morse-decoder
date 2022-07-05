7ccd087d414b76542d9c4c6f305f2a753608fd92
Kate Shpakovskaya <kate.shpakovskaya02@gmail.com>
2022-07-05 22:40:07 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -55,15 +55,24 @@ function decode(expr) {
     for (let i = 0; i < symbolArr.length; i++) {
         xArr = symbolArr[i].split('')
         var morseArr = ''
+
         for (let i = 0; i < 10; i++) {
           xArr[0] == '0' ? xArr.splice(0,1) : null   
-      }
-        for (let y = 0; y < 10; y+=2){
-          xArr[y+1] == '1' ? morseArr+= '-' : xArr[y+1] === '0'? morseArr+= '.' : morseAarr = ' ';
         }
+
+        for (let y = 0; y < xArr.length; y+=2){
+          xArr[y+1] === '1' ? morseArr+= '-' : xArr[y+1] === '0'? morseArr+= '.' : morseArr = ' ';
+        }
+
+        for (var s in MORSE_TABLE){
+            if(s === morseArr){
+              morseArr = MORSE_TABLE[s]
+            }
+
         symbolArr[i] = morseArr;
       }
-
+    }
+    return symbolArr.join('')
 }
 
 module.exports = {
